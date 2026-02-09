1bd2205da66aebfc766a28cbcfbf87c545c31bb6
homemade vhs <matthwatson@gmail.com>
2026-02-08 18:42:12 -0800

    Stronger drag blur (24px), original card stays highlighted at 50% opacity (v1.1.8)

--- src/settings.js
+++ src/settings.js
@@ -203,7 +203,7 @@ function renderSettings() {
     html += '</div>';
 
     // ---- Version ----
-    html += '<div class="settings-version">CUBBY v1.1.7</div>';
+    html += '<div class="settings-version">CUBBY v1.1.8</div>';
 
     container.innerHTML = html;
 
