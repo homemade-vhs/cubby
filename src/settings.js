72ccf4374e98f2eea30b76e24567e125a4790f90
homemade vhs <matthwatson@gmail.com>
2026-02-22 18:28:39 -0800

    Left-align content layout like Asana/Monday instead of centered

--- src/settings.js
+++ src/settings.js
@@ -202,7 +202,7 @@ function renderSettings() {
     html += '</div>';;
 
     // ---- Version ----
-    html += '<div class="settings-version">CUBBY v1.5.4</div>';
+    html += '<div class="settings-version">CUBBY v1.5.5</div>';
 
     container.innerHTML = html;
 
